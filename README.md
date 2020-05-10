653c5faa0bea8975973fc0e8b943ed93e0fcd9ce
thamyresmoraes <thamyresmoraes@live.com>
2020-05-10 16:53:08 -0300

    python_iniciante

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # estudos_python_flask
-Dedicado aos meus estudos de python com o framwork flask
+Dedicado aos meus estudos de python + framework flask;
+
+Pré-requisito:
+    - Python 3 instalado;
+
+Para executar um arquivo .py:
+    - No terminal na pasta onde está o arquivo digite `python nome_do_arquivo.py'`
+
+As aulas de python para iniciante e pyhton avançado estão nos arquivos .py `python_iniciante.py` e python `python_avançado.py` com os seguintes conteúdos:
+
+`python_iniciante`
+  - Variáveis;
+  - Printar varaável;
+  - Soma das variáveis e print do resultado;
+  - Subscrever variável;
+  - Criando string;
+  - Destacar uma letra dentro de uma string;
+  - Variáveis grandes;
+  - Tipos de variáveis;
+  - Operadores aritméticos;
+  - Operadores de comparação;
+  - Operadores Lógicos;
+  - Métodos python;
+  - Listas, tuplas e sets;
+  - Condicionais (if e else);
+  - Loops (while e for);
+
+ `python_avançado`
+  - Compreensão de lista;
+  - Dicionários;
+  - Objetos e classes;
+  - Herença;
+  - Métodos de classes e Métodos estáticos;
+  - `*args e **kargs`
+  - Decoradores
---
+++ python_iniciante.py
@@ -0,0 +1,188 @@
+
+
+"""
+
+    PYTHON INICIANTE
+
+"""
+
+#variáveis
+print('Declaração de variáveis: ')
+a = 2
+b = 4
+print('a = ', a)
+print('b = ', b)
+print('')
+
+#printar variavel
+print('printar as variáveis: ')
+print("O valor de 'a' é: ", a)
+print("O valor de 'b' é: ", b)
+print('')
+
+#soma e print do resultado
+print('Soma das variaveis declaradas')
+print("O resultado da soma é: ", a+b)
+print('')
+
+#subscrever variável
+print('Subscrevendo a variável a para 5:')
+a = 5
+print("a = 5")
+print("O valor de A agora é:", a)
+print('')
+
+#Criando string
+print('Criando string:')
+print("c = '3'")
+c = "3 é uma string"
+print("Printando string:", c)
+print()
+
+#Pontos de atenção
+print('###### Pontos de atenção: ######')
+print('')
+print('- não é possivel somar string com numero')
+print('- É possivel somar string com string')
+print('')
+
+#destacar uma letra dentro de uma string
+print("Destacar uma letra dentro de uma string: ")
+print("A letra 'b' está entre aspas simples")
+
+#o contra barra anula o próximo caracter
+print("A letra \"b\" esta entre aspas com contra barra")
+print()
+
+
+#por convenção escreva variaveis "grandes"  com underline (_)
+print("Por convenção escreva variaveis 'grandes'  com underline (_)")
+print("minha_variavel_grande = 'Dentro da convenção'")
+minha_variavel_grande = 'Dentro da convenção'
+print()
+
+#tipos de variáveis
+print("Tipos de variáveis:")
+print("alpha = 10 -> inteiro")
+print("bravo = \"uma string\"")
+print("charlie = 10.2 -> float")
+print("delta = True -> boolean")
+
+alpha = 10 #inteiro
+bravo = "uma string"
+charlie = 10.2 #float
+delta = True #boolean
+
+print()
+print()
+
+
+"""
+    OPERADORES ARITMÉTICOS
+
+"""
+
+print("###### OPERADORES ARITMÉTICOS ######")
+print("+ Soma")
+print("- Subtrai")
+print("/ Divide")
+print("* Multiplica")
+print("% Resto da divisão")
+print("3 ** 2 Valor ao quadrado, neste caso 3 ao quadrado")
+print("3 ** 3 Valor ao cubo")
+
+"""
+    OPERADORES DE COMPARAÇÃO
+
+"""
+
+print("###### OPERADORES DE COMPARAÇÃO ######")
+print("b == 2 --> Para comparar se é igual")
+print("b != 2 --> Para comparar se é diferente")
+print("b > 2 --> Para comparar se é maior")
+print("b < 2 --> Para comparar se é menor")
+print("b >= 2 --> Para comparar se é maior ou igual")
+print("b <= 2 --> Para comparar se é menor ou igual")
+
+
+"""
+    OPERADORES LÓGICOS
+
+"""
+print("###### OPERADORES LÓGICOS ######")
+A = True
+B = False
+print()
+
+
+print("A = True")
+print("B = False")
+print()
+
+print("------- A E B ---------")
+print("Resultado de A and B: ", A and B)
+print()
+print("Como o B é false, então a condição é falsa. Para que seja verdadeira, as duas variáveis precisam ser verdadeiras")
+print()
+
+
+A = True
+B = True
+print()
+
+
+print("A = True")
+print("B = True")
+print("Resultado de A and B: ", A and B)
+
+print("------- A OU B ---------")
+
+A = True
+B = False
+print()
+
+
+print("A = True")
+print("B = False")
+print("O Resultado A or B:", A or B)
+
+print()
+print("Precisamos que apenas um seja verdadeiro para a condição retornar True")
+print()
+
+
+print("------- NOT A ---------")
+
+A = True
+B = False
+print()
+
+
+print("A = True")
+print("B = False")
+print("Resultado not A: ", not A)
+
+print()
+print("Retorna o resultado inverso!")
+print()
+
+print("------- NOT B ---------")
+
+A = True
+B = False
+print()
+
+
+print("A = True")
+print("B = False")
+print("Resultado not B: ", not B)
+
+print()
+print("Retorna o resultado inverso!")
+print()
+
+"""
+    MÉTODOS EM PYTHON
+
+"""
+print("###### MÉTODOS EM PYTHON ######")
