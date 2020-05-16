9b0f0178f66ee7926c0f9764a65651b3f0dbc580
thamyresmoraes <thamyresmoraes@live.com>
2020-05-16 18:25:33 -0300

    implementando put

--- travel/resources/hotel.py
+++ travel/resources/hotel.py
@@ -29,20 +29,26 @@ class Hoteis(Resource):
         return {'hoteis': hoteis}
 
 class Hotel(Resource):
-    def get(self, hotel_id):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
         return {'message': 'Hotel not found.'}, 404 #not found
 
     def post(self, hotel_id):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('nome')
-        argumentos.add_argument('estrelas')
-        argumentos.add_argument('diaria')
-        argumentos.add_argument('cidade')
-
-        dados = argumentos.parse_args()
+        dados = Hotel.argumentos.parse_args()
 
         novo_hotel = {
             'hotel_id': hotel_id,
@@ -57,7 +63,15 @@ class Hotel(Resource):
         return novo_hotel, 200
 
     def put(self, hotel_id):
-        pass
+        dados = Hotel.argumentos.parse_args()
+        novo_hotel = { 'hotel_id': hotel_id, **dados }
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200 #atualizado
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201 #created
 
     def delete(self, hotel_id):
         pass
