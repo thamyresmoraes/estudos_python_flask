f0cd9ecbac14d009ebf3038d3869d6f1a9c39025
thamyresmoraes <thamyresmoraes@live.com>
2020-05-16 17:36:24 -0300

    retornando uma lista de hoteis

--- travel/app.py
+++ travel/app.py
@@ -4,9 +4,34 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'Alpha Hotel',
+        'estrelas': 4.4,
+        'diaria': 420.31,
+        'cidade': 'São Paulo'
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel',
+        'estrelas': 4.3,
+        'diaria': 380.34,
+        'Cidade': 'Ilhéus'
+    },
+    {
+        'hotel_id': 'charlie',
+        'nome': 'Charlie Hotel',
+        'estrelas': 3.9,
+        'diaria': 250.34,
+        'cidade': 'Itacaré'
+    },
+]
+
+
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': 'meus hoteis'}
+        return {'hoteis': hoteis}
 
 api.add_resource(Hoteis, '/hoteis')
 
