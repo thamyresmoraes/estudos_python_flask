701356afe1f73318bd990e9002ea5f9d2f03b4ef
thamyresmoraes <thamyresmoraes@live.com>
2020-05-23 19:51:16 -0300

    cria_banco_auto_com_sqlalchemy

--- travel/models/hotel.py
+++ travel/models/hotel.py
@@ -1,3 +1,13 @@
+from sql_alchemy import banco
+
+class HotelModel(banco.Model):
+    __tablename__ = 'hoteis'
+
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=1))
+    diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
 
 class HotelModel:
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
