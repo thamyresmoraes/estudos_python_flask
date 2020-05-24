a8ad54be7178a59c7cefa5bda58705023359aa20
thamyresmoraes <thamyresmoraes@live.com>
2020-05-24 16:53:33 -0300

    requerendo autorização para executar ação

--- travel/resources/hotel.py
+++ travel/resources/hotel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
+from flask_jwt_extended import jwt_required
 
 
 class Hoteis(Resource):
@@ -19,6 +20,7 @@ class Hotel(Resource):
             return hotel.json()
         return {'message': 'Hotel not found.'}, 404 #not found
 
+    @jwt_required
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
             return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400
@@ -31,7 +33,7 @@ class Hotel(Resource):
             return {'message': 'An internal error occured to save hotel.'}, 500
         return hotel.json()
 
-
+    @jwt_required
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
         hotel_encontado = HotelModel.find_hotel(hotel_id)
@@ -46,6 +48,7 @@ class Hotel(Resource):
             return {'message': 'An internal error occured to save hotel.'}, 500
         return hotel.json(), 201
 
+    @jwt_required
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
--- travel/resources/usuario.py
+++ travel/resources/usuario.py
@@ -1,7 +1,9 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required
 from werkzeug.security import safe_str_cmp
+
+
 
 atributos = reqparse.RequestParser()
 atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
@@ -15,7 +17,7 @@ class User(Resource):
             return user.json()
         return {'message': 'User not found.'}, 404 #not found
 
-
+    @jwt_required
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
